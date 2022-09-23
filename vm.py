da9a017b49e54694921233bac57d97c60c131b47
KJ <kernel1983@gmail.com>
2022-09-23 21:24:59 +0800

    POP_TOP STORE_SUBSCR RETURN_VALUE LOAD_CONST BUILD_MAP STORE_FAST CALL_METHOD

--- vm.py
+++ vm.py
@@ -2,6 +2,7 @@
 import dis
 import codeop
 import hashlib
+import functools
 
 import stf
 
@@ -26,7 +27,7 @@ def chain_stf(state, data):
 
 dis.dis(stf.chain_stf)
 print('co_code', [hex(i) for i in stf.chain_stf.__code__.co_code])
-# print(opcode.opmap)
+# print(stf.chain_stf.__code__)
 
 print('co_name', stf.chain_stf.__code__.co_name)
 print('co_varnames', stf.chain_stf.__code__.co_varnames)
@@ -52,9 +53,15 @@ class VM:
 
         self.pc = 0
         self.stack = []
-        self.memory = []
+        self.vars = {}
 
     def run(self, function_name, args):
+        assert len(args) == self.co_argcount
+        self.args = args
+        for i, v in enumerate(self.args):
+            self.vars[self.co_varnames[i]] = v
+        print('---')
+
         pc = -1
         while pc != self.pc:
             pc = self.pc
@@ -64,48 +71,84 @@ class VM:
 
     def step(self):
         print(self.pc, hex(self.co_code[self.pc]))
+        print('vars', self.vars)
         if self.co_code[self.pc] == 0x0:
             print()
 
         elif self.co_code[self.pc] == 0x1: # POP_TOP
+            print('POP_TOP')
+            self.stack.pop()
             self.pc += 2
 
         elif self.co_code[self.pc] == 0x3c: # STORE_SUBSCR
+            key = self.stack.pop()
+            obj = self.stack.pop()
+            val = self.stack.pop()
+            print('STORE_SUBSCR', obj, '[', key, '] =', val)
+
+            left = self.vars[obj]
+            right = self.vars[val]
+            left[key] = right
+            # self.stack.append(left)
             self.pc += 2
 
         elif self.co_code[self.pc] == 0x53: # RETURN_VALUE
-            return 'value'
+            print('RETURN_VALUE')
+            val = self.stack[-1]
+            var = self.vars[val]
+            return var
 
         elif self.co_code[self.pc] == 0x64: # LOAD_CONST
+            param = self.co_code[self.pc+1]
+            print('LOAD_CONST', param)
+            self.stack.append(self.co_consts[param])
             self.pc += 2
 
         elif self.co_code[self.pc] == 0x69: # BUILD_MAP
+            param = self.co_code[self.pc+1]
+            print('BUILD_MAP', param)
+            if param == 0:
+                self.stack.append({})
             self.pc += 2
 
         elif self.co_code[self.pc] == 0x7c: # LOAD_FAST
             param = self.co_code[self.pc+1]
-            self.stack.append(self.co_varnames[param])
             print('LOAD_FAST', param)
-            print('stack', self.stack)
+            var = self.co_varnames[param]
+            self.stack.append(var)
             self.pc += 2
 
         elif self.co_code[self.pc] == 0x7d: # STORE_FAST
             param = self.co_code[self.pc+1]
             print('STORE_FAST', param)
+            # print('STORE_FAST', self.co_varnames[param])
+            var = self.co_varnames[param]
+            val = self.stack.pop()
+            self.vars[var] = val
             self.pc += 2
 
         elif self.co_code[self.pc] == 0xa0: # LOAD_METHOD
             param = self.co_code[self.pc+1]
-            self.stack.append(self.co_names[param])
             print('LOAD_METHOD', param)
-            print('stack', self.stack)
+            self.stack.append(self.co_names[param])
             self.pc += 2
 
         elif self.co_code[self.pc] == 0xa1: # CALL_METHOD
+            param = self.co_code[self.pc+1]
+            print('CALL_METHOD', param)
+            # print('CALL_METHOD', self.stack[-2-param])
+            var = self.vars[self.stack[-2-param]]
+            method = self.stack[-1-param]
+            params = self.stack[-param:]
+            result = functools.partial(var.__getattribute__(method), *params)()
+            print('result', result)
+            self.stack = self.stack[:-2-param]
+            self.stack.append(result)
             self.pc += 2
 
+        print('stack', self.stack)
         print('---')
 
 vm = VM()
 vm.import_function(stf.chain_stf)
-vm.run('chain_stf', [1, 2])
+vm.run('chain_stf', [{}, {'subchains': {1:2}}])
